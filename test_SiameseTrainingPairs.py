c4d1682fba1204720ef60ea8f5d100bb156ef32d
EddieB <eddiebergmanhs@gmail.com>
2020-06-22 12:37:35 +0100

    Changed to Pytest

--- test_SiameseTrainingPairs.py
+++ test_SiameseTrainingPairs.py
@@ -1,4 +1,5 @@
-from siamese_net import SiameseTrainingPairs
+import torch
+import pytest
 from itertools import combinations
 
 """
@@ -12,31 +13,89 @@ TEST 138757229: SiameseTrainingPairs
 
    Also checked is that the similarity function works across them.
 """
-samples = ['a', 'b', 'c', 'd']
-classes = [1, 0, 2, 2]
-same_class = lambda class_a, class_b: int(class_a == class_b)
+from .siamese_net import SiameseTrainingPairs
 
-sample_combinations = list(combinations(samples, 2))
-classes_combinations = list(combinations(classes, 2))
-expected_similarities = [same_class(a, b) for a, b in classes_combinations]
+class TestSiameseTrainingPairs:
 
-similarity_func = lambda s_a, m_a, s_b, m_b: same_class(m_a, m_b)
-dataset = SiameseTrainingPairs(samples, classes, similarity_func)
+    @staticmethod
+    def _same_class(a, b, class_a, class_b):
+        return int(class_a == class_b)
 
-zipped_items = list(zip(dataset, sample_combinations, expected_similarities))
+    def test_init(self):
+        with pytest.raises(ValueError):
+            dataset = SiameseTrainingPairs(samples=['a', 'b'],
+                                           sample_info=[0, 1, 1],
+                                           similarity_func=self._same_class)
 
-# Assert length of dataset is correct
-assert len(dataset) == len(sample_combinations), \
-    f'{len(dataset)=} != {len(sample_combinations)}='
+    def test_len(self):
+        dataset = SiameseTrainingPairs(samples=['a', 'b', 'c'],
+                                       sample_info=[0, 1, 1],
+                                       similarity_func=self._same_class)
 
-# Assert order of items and similarity is calculated correctly
-for ds_item, correct_pair, expected_similarity in zipped_items:
-    assert ds_item == (expected := (*correct_pair, expected_similarity)), \
-        f'{ds_item=} != {expected=}'
+        possible_pairs = [('a', 'b'), ('a', 'c'), ('b', 'c')]
+        assert len(dataset) == len(possible_pairs)
 
-# Assert indexing works as required
-for i in range(0, len(sample_combinations)):
-    ds_item = dataset[i]
-    expected = (*sample_combinations[i], expected_similarities[i])
-    assert ds_item == expected, f'{ds_item=} != {expected=}'
+    def test_getitem(self):
+        samples = ['a', 'b', 'c']
+        classes = [0, 1, 1]
+        sim_f = self._same_class
+        dataset = SiameseTrainingPairs(samples, classes, sim_f)
 
+        # Ordered lexographically
+
+        sample_pairs = list(combinations(samples, 2))
+        class_pairs = list(combinations(classes, 2))
+
+        similarities = [
+            sim_f(a, b, class_a, class_b)
+            for (a, b), (class_a, class_b) in zip(sample_pairs, class_pairs)
+        ]
+
+        for item, (a, b), sim in zip(dataset, sample_pairs, similarities):
+            assert item == (a, b, sim)
+
+"""
+Test 1890846940: ContrastiveLoss
+
+Testing contrastive loss for a few different examples. This considers all
+distances to be scaled [0, 1] which is possible with a maximum bound B
+such that dist(x, y) <= B.
+
+This is more to serve as a look at the function results rather than
+a serious test. We choose the points as being on a simplex which
+have a bound such that dist(x, y) <= sqrt(2).
+
+    Margin = 0.5
+    Similar - similarity(x, y) = 0
+        - dist(x, x) = 0
+        - dist(x, y) = 0.2 < margin
+        - dist(x, z) = 0.7 > margin
+
+    Disimilar - similarity(x, y) = 1
+        - dist(x, x) = 0
+        - dist(x, y) = 0.2 < margin
+        - dist(x, z) = 0.7 > margin
+
+    Slightly Similar - similarity(x, y) = 0.25
+        - dist(x, x) = 0
+        - dist(x, y) = 0.2 < margin
+        - dist(x, z) = 0.7 > margin
+"""
+from .siamese_net import ContrastiveLoss
+import math
+
+x = torch.Tensor([1.0, 0.0])
+y = torch.Tensor([(10 - 1*math.sqrt(2)) / 10,  1*math.sqrt(2) / 10])
+z = torch.Tensor([(20 - 7*math.sqrt(2)) / 20,  7*math.sqrt(2) / 20])
+
+lefts = torch.stack([x, x, x])
+rights = torch.stack([x, y, z])
+print(f'{lefts=}')
+print(f'{rights=}')
+
+loss_f = ContrastiveLoss(margin=0.5)
+for sim in [0.0, 1.0, 0.25]:
+    length = len(lefts)
+    sims = torch.tensor(sim).repeat(length)
+    mean_losses = loss_f(lefts, rights, sims)
+    print(f'{mean_losses=}')
